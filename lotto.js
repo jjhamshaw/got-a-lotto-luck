5563d63c49d29717bf35cce9501b7ae5d31e5b62
jon <jonathanhamshaw@gmail.com>
2012-11-12 22:52:00 +0000

    refactoring js out of html

--- lotto.js
+++ lotto.js
@@ -8,6 +8,18 @@ function get_lotto_numbers(num){
 	return numbers;
 };
 
+function create_lotto_listItems(){
+	var numbers = get_lotto_numbers(6);
+	var list = "";
+	var unorderedList = document.getElementById('lottoNumbers');
+
+	for (var i = 0; i < numbers.length; i++) {
+		list += "<li>"  + numbers[i] + "</li>";
+	}
+
+	return list;
+};
+
 var generateLottoNumber = function(){
 	return Math.floor(Math.random() *49 +1);
 };
